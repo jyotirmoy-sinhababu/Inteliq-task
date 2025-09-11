fbb70ec34d319105c3ac2a99021db1a0296aa44e
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2025-09-12 01:35:51 +0530

    first layout created

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+import ChatArea from './pages/ChatArea';
+// import Sidebar from './components/Sidebar';
+// import Layout from './pages/Layout';
+function App() {
+  return (
+    <>
+      {/* <Sidebar /> */}
+      {/* <Layout /> */}
+      <ChatArea />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/LogoAndIcons.tsx
@@ -0,0 +1,9 @@
+const Logo = () => {
+  return (
+    <div>
+      <img src='' alt='' />
+    </div>
+  );
+};
+
+export default Logo;
